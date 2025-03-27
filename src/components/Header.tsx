7be73ab33e0fd69adf5ff2eb669de92c497f830f
anji0114 <anji.tnk.140114@gmail.com>
2025-03-27 13:31:01 +0900

    setting logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export const Header = () => {
   return (
     <header className="w-full  backdrop-blur-sm border-b border-slate-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           <div className="flex items-center">
-            <span className="ml-2 text-2xl font-bold text-white">DeepDaiv</span>
+            <Link href="/" className="flex items-center brightness-100 invert">
+              <Image src="/logo.svg" alt="logo" width={140} height={34} />
+            </Link>
           </div>
           <div className="flex items-center gap-2">
             <button className="flex items-center px-5 py-2 rounded-lg border border-gray-600 text-white">
